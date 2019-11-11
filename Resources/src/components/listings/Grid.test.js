bdc53c3c53219af070cfaa7d5e7bef30e5ed49f8
Nick Garza <adagar121987@gmail.com>
2019-11-11 16:51:40 -0500

    Using describe to organize tests

--- Resources/src/components/listings/Grid.test.js
+++ Resources/src/components/listings/Grid.test.js
@@ -2,20 +2,37 @@ import data from '../../data/courses.json';
 
 const numItems = data.length;
 
-test('Number of items = 12', () => {
-  expect(numItems).toBe(12);
+describe('Number tests', () => {
+  test('Number of items = 12', () => {
+    expect(numItems).toBe(12);
+  });
+
+  test('Number of items to be greater than 12', () => {
+    expect(numItems).toBeGreaterThanOrEqual(12);
+  });
 });
 
-test('Number of items to be greater than 12', () => {
-  expect(numItems).toBeGreaterThanOrEqual(12);
+describe('String tests', () => {
+  const dataTest = data[0].title;
+
+  test('There is a JS in this title', () => {
+    expect(dataTest).toMatch(/JS/);
+  });
+
+  test('The title contains React', () => {
+    expect(dataTest).toContain('React');
+  });
 });
 
-const dataTest = data[0].title;
 
-test('There is a JS in this title', () => {
-  expect(dataTest).toMatch(/JS/);
-});
+describe('Arrays and objects', () => {
+  const data2 = ['React Native', 'MeteorJS'];
 
-test('The title contains React', () => {
-  expect(dataTest).toContain('React');
+  test('The list of course mentions react native and meteorjs', () => {
+    expect(['React Native', 'MeteorJS', 'React']).toEqual(expect.arrayContaining(data2));
+  });
+
+  test('The first course to have property of title', () => {
+    expect(data[0]).toHaveProperty('views', '31,266');
+  });
 });
